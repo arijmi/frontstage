7cac513320f65d16e87ccecdc951f1c1a7f9bc0d
nada matoussi <nada.matoussi@gmail.com>
2024-10-09 14:41:09 +0100

    commit3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AuthServiceService } from './auth-service.service';
-import { environment } from 'src/environments/environment';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { ReactiveFormsModule } from '@angular/forms';
-import { EventService } from './services/event.service';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    IonicModule.forRoot(), // Assurez-vous que ceci est ici
     AngularFireAuthModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     ReactiveFormsModule,
-    IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    AuthServiceService,
-    EventService,
-    LocalNotifications
-  ],
+  providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}

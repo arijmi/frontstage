b1483ca8ae7d7d013450ac2254add3c2668490dc
nada matoussi <nada.matoussi@gmail.com>
2024-10-10 11:06:33 +0100

    allo-Match1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AuthServiceService } from './auth-service.service';
 import { environment } from 'src/environments/environment';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { ReactiveFormsModule } from '@angular/forms';
-import {NgModule,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 @NgModule({
   declarations: [AppComponent],
-  
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig), // Initialisation correcte
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Utilisation de l'API compat
     AngularFireAuthModule,
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore())
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
